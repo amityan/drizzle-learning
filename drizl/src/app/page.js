5b81555567bf66b455e294d62cc7fffbc58fe187
Smituhf <153477048+amityan@users.noreply.github.com>
2024-05-21 04:07:44 +0000

    Commit

---
+++ drizl/src/app/page.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">DrizzleLearning</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
